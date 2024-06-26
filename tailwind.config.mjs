f72fb9919e9a1aa4c52cf6e8fcbf684d07e4b9c7
Max T. Kristiansen <me@maxtrier.dk>
2024-06-27 00:12:18 +0200

    feat: Customize resumé design for print

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
     theme: {
         extend: {
+            screens: {
+                print: { raw: 'print' }
+            },
             colors: {
                 primary: "#1658A6"
             }
